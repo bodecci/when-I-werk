15205cf4c002b6f96a5da7a02496f936b98baf3b
Bode Falade <bode.falade@outlook.com>
2019-06-03 23:30:16 -0500

    client side landing page added.

---
+++ src/components/App/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+// import {
+//   HashRouter as Router,
+//   Route,
+//   Redirect,
+//   Switch
+// } from 'react-router-dom';
+// import ProtectedRoute from "../ProtectedRoute/ProtectedRoute"
+// import {connect} from 'react-redux';
+import './App.css';
+
+class App extends Component {
+
+  render () {
+    return (
+        <div className="App">
+          <header><h1>Shift Creator</h1></header>
+          
+        </div>
+
+      
+    )
+  }
+}
+
+export default App;
